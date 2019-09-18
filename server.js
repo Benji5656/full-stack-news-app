7b132d61ebfe30d42b3c05fb33fcb249a5a463b9
[Benji] <benjamin_landis92@yahoo.com>
2019-09-18 08:43:02 -0500

    set up for news app

---
+++ server.js
@@ -0,0 +1,23 @@
+const bodyParser = require( 'body-parser' )
+const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+const app = express()
+const api = require('./server/routes/api')
+
+// serving files
+app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'node_modules')))
+
+app.use( bodyParser.json() )
+app.use( bodyParser.urlencoded( { extended: false } ) )
+
+
+// Mongoose setup
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/forecast', { useNewUrlParser: true})
+
+app.use('/', api)
+
+
+const port = 4320
+app.listen(process.env.PORT || port, () => console.log(`Running on port ${port}`))
---
+++ server/routes/api.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const router = express.Router()
+// const News = require('../models/NewsResults')
+const request = require('request')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router
