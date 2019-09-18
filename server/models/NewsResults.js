34fe3645be5424326b5df0fb291d1f81ecb2a4ee
Luis270100 <luisarthe27@gmail.com>
2019-09-18 11:42:08 -0500

    basic css

--- server/models/NewsResults.js
+++ server/models/NewsResults.js
@@ -14,7 +14,6 @@ const NewsResult = new Schema({
 
 
 const ClientInfo = new Schema({
-
     NewsResultId: [{ type: Schema.Types.ObjectId, ref: 'News' }],
     KeyWordSearch: [],
     saveLater: [{ type: Schema.Types.ObjectId, ref: 'News' }] //on click
