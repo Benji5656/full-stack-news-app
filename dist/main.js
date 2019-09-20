ab5b3324ec6a4b3160d1e9378cc185d10122626f
[Benji] <benjamin_landis92@yahoo.com>
2019-09-19 19:08:57 -0500

    no more y

--- dist/main.js
+++ dist/main.js
@@ -20,7 +20,7 @@ loadPage()
 
 
 
-$("body").on("click", ".bookmark", async function () {y
+$("body").on("click", ".bookmark", async function () {
     let title = $(this).siblings('.title').text()
     let description = $(this).siblings('.description').text()
     let url = $(this).siblings('.url').text()
